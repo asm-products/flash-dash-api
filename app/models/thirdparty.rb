96f6047b4a58f6610a442de272b094345e746873
Vinoth <a.vinothk@outlook.com>
2015-01-16 22:35:32 +0530

    add usernames during api call to DB

--- app/models/thirdparty.rb
+++ app/models/thirdparty.rb
@@ -6,4 +6,13 @@ class Thirdparty < ActiveRecord::Base
   validates_presence_of :user_id
   validates_presence_of :service_id
   
+  def self.verify(service, current_user, username)
+    site = Service.find_by_service(service)
+    if current_user.thirdparties.find_by_username(username)
+      return username
+    else
+      Thirdparty.create(:username => username, :oauth => nil, :service => site, :user => current_user)
+    end
+  end
+
 end
